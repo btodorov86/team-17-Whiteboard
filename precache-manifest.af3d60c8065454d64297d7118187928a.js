d4cb15bb9e9a743e9ade08f7eaf2a70e34607261
Borislav Todorov <60582465+btodorov86@users.noreply.github.com>
2020-09-23 17:04:47 +0300

    Updates

---
+++ precache-manifest.af3d60c8065454d64297d7118187928a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26be710e78e5cff1e030b6edda995910",
+    "url": "/team-17-Whiteboard/index.html"
+  },
+  {
+    "revision": "3142a8eaa0558ab61bba",
+    "url": "/team-17-Whiteboard/static/css/2.29c4d543.chunk.css"
+  },
+  {
+    "revision": "8973df31ac6a8132b205",
+    "url": "/team-17-Whiteboard/static/css/main.95194710.chunk.css"
+  },
+  {
+    "revision": "3142a8eaa0558ab61bba",
+    "url": "/team-17-Whiteboard/static/js/2.2a0a00fa.chunk.js"
+  },
+  {
+    "revision": "4a28eb47e795bd2cd0f181d5a36fb1a6",
+    "url": "/team-17-Whiteboard/static/js/2.2a0a00fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8973df31ac6a8132b205",
+    "url": "/team-17-Whiteboard/static/js/main.cf6b51ab.chunk.js"
+  },
+  {
+    "revision": "0b931b2e416fc63d61a4",
+    "url": "/team-17-Whiteboard/static/js/runtime-main.4189c402.js"
+  },
+  {
+    "revision": "77f2d328ed1f05eba565ae7eb1f13084",
+    "url": "/team-17-Whiteboard/static/media/500-internal-server-error.77f2d328.png"
+  },
+  {
+    "revision": "8ca3c397501271b477b91163b62e0233",
+    "url": "/team-17-Whiteboard/static/media/earth.8ca3c397.PNG"
+  },
+  {
+    "revision": "6d200ff6cd48a5f0eb8f1d19aaac8711",
+    "url": "/team-17-Whiteboard/static/media/image404.6d200ff6.PNG"
+  },
+  {
+    "revision": "d1cf4d8c3e0d7c77f20572a9b539a519",
+    "url": "/team-17-Whiteboard/static/media/img11.d1cf4d8c.png"
+  }
+]);
